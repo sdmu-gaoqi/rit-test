887812cdf18533c513c4615d42aa6e5afe9503a5
gaoqi <1224362143@qq.com>
2024-03-29 17:00:33 +0800

    ci: 修改ci命令

---
+++ scripts/release-it.sh
@@ -0,0 +1,15 @@
+NPMTOKEN=$1
+ACTIONTOKEN=$2
+GITBRANCH=$3
+
+getMessageInfo () {
+ echo '开始执行'
+ echo ${NPMTOKEN}
+ echo ${GITBRANCH}
+
+ git fetch --all
+ git checkout ${GITBRANCH}
+ git reemote -v
+}
+
+getMessageInfo
