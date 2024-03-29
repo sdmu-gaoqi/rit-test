33289a66b27550925766e393466ad40705c2d10c
gaoqi <1224362143@qq.com>
2024-03-29 18:05:47 +0800

    ci: 修改脚本信息

--- scripts/release-it.sh
+++ scripts/release-it.sh
@@ -5,13 +5,13 @@ gitCheckBranch () {
  echo ${GITBRANCH}
 
  git fetch --all
-#  git checkout ${GITBRANCH}
-#  git remote -v
+ git checkout ${GITBRANCH}
+ git remote -v
 
- REMOTEVERSION=npm view | grep latest | awk -F: '{ print $2 }'
- PACKAGEVERSION=npm pkg get version | sed 's/[",]//g'
- echo 当前npm包 最新版本为 ${REMOTEVERSION}
- echo 当前npm包 本地版本为 ${PACKAGEVERSION}
+#  REMOTEVERSION=npm view | grep latest | awk -F: '{ print $2 }'
+#  PACKAGEVERSION=npm pkg get version | sed 's/[",]//g'
+#  echo 当前npm包 最新版本为 ${REMOTEVERSION}
+#  echo 当前npm包 本地版本为 ${PACKAGEVERSION}
 }
 
 release() {
@@ -19,3 +19,4 @@ release() {
 }
 
 gitCheckBranch
+release
