3b48db84dd31c989570a4e9f5596013c834a7778
gaoqi <1224362143@qq.com>
2024-03-29 17:36:47 +0800

    ci: 修改脚本

--- scripts/release-it.sh
+++ scripts/release-it.sh
@@ -1,13 +1,21 @@
 GITBRANCH=$1
 
-getMessageInfo () {
+gitCheckBranch () {
  echo '开始执行'
  echo ${GITBRANCH}
 
  git fetch --all
  git checkout ${GITBRANCH}
  git remote -v
- git log
+
+ REMOTEVERSION = npm view | grep latest | awk -F: '{ print $2 }'
+ PACKAGEVERSION = npm pkg get version | sed ’s/[“,]//g’
+ echo 当前npm包 最新版本为 ${REMOTEVERSION}
+ echo 当前npm包 本地版本为 ${PACKAGEVERSION}
 }
 
-getMessageInfo
+release() {
+    npm release-it --ci
+}
+
+gitCheckBranch
