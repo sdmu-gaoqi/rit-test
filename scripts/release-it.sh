a82eb3021554ab879ca76f4d6f626b1909a7ee5e
gaoqi <52244292+sdmu-gaoqi@users.noreply.github.com>
2024-04-02 19:57:35 +0800

    chore: 修改脚本 (#20)
    ci: 修改ci命令

---
+++ scripts/release-it.sh
@@ -0,0 +1,22 @@
+GITBRANCH=$1
+
+gitCheckBranch () {
+ echo '开始执行'
+ echo ${GITBRANCH}
+
+ git fetch --all
+ git checkout ${GITBRANCH}
+ git remote -v
+
+#  REMOTEVERSION=npm view | grep latest | awk -F: '{ print $2 }'
+#  PACKAGEVERSION=npm pkg get version | sed 's/[",]//g'
+#  echo 当前npm包 最新版本为 ${REMOTEVERSION}
+#  echo 当前npm包 本地版本为 ${PACKAGEVERSION}
+}
+
+release() {
+    yarn run release
+}
+
+gitCheckBranch
+release
